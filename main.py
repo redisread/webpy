365186d691f8c3d237aa1e06b3853c597db86923
victorhongdream <1427298682@qq.com>
2020-03-27 23:05:07 +0800

    new

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app = web.application(urls, globals())
 
 class hello:
     def GET(self):
-        return 'Hello, world!'
+        msg = '''
+        <h1>Web.py</h1>
+        Hello,web.py!<br><br>
+        I am Victor!<br>
+        '''
+        return msg
 
 if __name__ == "__main__":
     app.run()
