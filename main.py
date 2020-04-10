83c9971de8947c153d4fbe3f718be3f05c45fc15
unknown <wujiahong2013@hotmail.com>
2020-04-10 22:38:13 +0800

    new

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import web
 import test
 from todo_app import todo
 from blog_app import blog
-# # 指定模板目录
-# render = web.template.render('templates')
-# # 连接数据库
-# db = web.database(dbn='mysql', user='root', pw='123456', db='test',host='90b514053e09c9c7.c.cloudtogo.cn',port=38008)
+# 指定模板目录
+render = web.template.render('templates')
+# 连接数据库
+db = web.database(dbn='mysql', user='root', pw='123456', db='test',host='90b514053e09c9c7.c.cloudtogo.cn',port=38008)
 
 # 定义网页的所有地址
 urls = (
     '/','Index',
     '/test',test.app_test,
     '/todo',todo.app_todo,
-    '/blog',blog.app_blog
+    '/blog',blog.app_blog,
+    '/button','Button'
 )   
 
 render = web.template.render("templates")
@@ -20,8 +21,11 @@ render = web.template.render("templates")
 # 网页访问的处理类
 class Index:
     def GET(self):
-        return render.index()
-        
+        return render.button() 
+
+class Button:
+    def GET(self):
+         return "sd"
 
 if __name__ == "__main__":  
     # 新建网页App
